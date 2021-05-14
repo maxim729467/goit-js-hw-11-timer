4ecb05cefce15f0826fa0be77adf89ddb022ed22
Maksym Shyshko <maxim.shishko95@gmail.com>
2021-05-15 00:00:19 +0300

    updated script

--- src/index.js
+++ src/index.js
@@ -8,6 +8,24 @@ class CountdownTimer {
         this.startCount();
     }
 
+    convertTime(time) {
+const pad = function (value) {
+                return String(value).padStart(2, '0');
+        };
+        
+         const calculatedTime = function (time) {
+        const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+        return {days, hours, mins, secs };
+        };
+        
+        return calculatedTime(time);
+
+    }
+
     init() {
     const timerBlock = document.querySelector(`${this.selector}`);
     const plugin = {
@@ -16,19 +34,6 @@ class CountdownTimer {
       minute: timerBlock.children[2].firstElementChild,
       second: timerBlock.children[3].firstElementChild,
 };
-            
-            const pad = function (value) {
-                return String(value).padStart(2, '0');
-            };
-
-        const calculatedTime = function (time) {
-        const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-        return {days, hours, mins, secs };
-    };
 
             if (this.targetDate <= new Date()) {
             plugin.day.textContent =    "00";
@@ -38,7 +43,7 @@ class CountdownTimer {
             return;
         }
         
-            let remainingTime = calculatedTime(this.targetDate - new Date());
+            let remainingTime = this.convertTime(this.targetDate - new Date());
             plugin.day.textContent = remainingTime.days;
             plugin.hour.textContent = remainingTime.hours;
             plugin.minute.textContent = remainingTime.mins;
@@ -56,25 +61,12 @@ class CountdownTimer {
       minute: timerBlock.children[2].firstElementChild,
       second: timerBlock.children[3].firstElementChild,
 };
-            
-            const pad = function (value) {
-                return String(value).padStart(2, '0');
-            };
-
-        const calculatedTime = function (time) {
-        const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-        return {days, hours, mins, secs };
-    };
 
             if (this.targetDate <= new Date()) {
                 clearInterval(timerActivity);
                 return;
      }
-            let remainingTime = calculatedTime(this.targetDate - new Date());
+            let remainingTime = this.convertTime(this.targetDate - new Date());
             plugin.day.textContent = remainingTime.days;
             plugin.hour.textContent = remainingTime.hours;
             plugin.minute.textContent = remainingTime.mins;
@@ -87,7 +79,7 @@ class CountdownTimer {
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jun 01, 2021'),
+  targetDate: new Date('Jun 1, 2021'),
 });
 
 
